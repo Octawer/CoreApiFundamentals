8ec25cb5a839d6165bcffd2a40dd974d45717360
Octavio Martinez <o.martinez.gar@gmail.com>
2021-07-06 15:45:53 +0200

    camps controller, GET (wip), models, mapper

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _campRepository;
+        private readonly IMapper _mapper;
+
+        public CampsController(ICampRepository campRepository, IMapper mapper)
+        {
+            _campRepository = campRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<CampModel[]> GetAsync() 
+        {
+            var camps = await _campRepository.GetAllCampsAsync();
+
+            return _mapper.Map<CampModel[]>(camps);
+        }
+    }
+}
---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(cm => cm.Venue, x => x.MapFrom(c => c.Location.VenueName));
+        }
+    }
+}
